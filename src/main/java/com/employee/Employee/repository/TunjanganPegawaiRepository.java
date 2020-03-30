6f892eaa1bbe4570416f142c28eb094b21f966ab
yuslamrl_31 <yuslamrl@gmail.com>
2020-03-30 16:36:11 +0700

    Update Repository Parameter, Tunjangan_Pegawai.

--- src/main/java/com/employee/Employee/repository/TunjanganPegawaiRepository.java
+++ src/main/java/com/employee/Employee/repository/TunjanganPegawaiRepository.java
@@ -1,9 +1,10 @@
 package com.employee.Employee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.employee.Employee.model.TunjanganPegawai;
 
+@Repository
 public interface TunjanganPegawaiRepository extends JpaRepository<TunjanganPegawai, Integer> {
 
 }
