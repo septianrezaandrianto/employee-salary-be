5978369f9745ad3eded3812b132055c4cbeae588
Fazri Deffara <mfazrideffara@gmail.com>
2020-03-30 10:35:28 +0700

    Update Repository

---
+++ src/main/java/com/employee/Employee/repository/AgamaRepository.java
@@ -0,0 +1,9 @@
+package com.employee.Employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.employee.Employee.model.Agama;
+
+public interface AgamaRepository extends JpaRepository<Agama, Long>{
+
+}
