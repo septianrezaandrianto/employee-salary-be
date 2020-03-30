de8c05ff4e0790654912a2e10f052eb06c5519ff
Fazri Deffara <mfazrideffara@gmail.com>
2020-03-30 10:38:21 +0700

    Update DTO

--- src/main/java/com/employee/Employee/dto/KaryawanDTO.java
+++ src/main/java/com/employee/Employee/dto/KaryawanDTO.java
@@ -32,5 +32,145 @@ public class KaryawanDTO implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 
+	public KaryawanDTO(int idKaryawan, PosisiDTO posisiDto, PenempatanDTO penempatanDto, TingkatanDTO tingkatanDto,
+			AgamaDTO agamaDto, String nama, String noKtp, String alamat, Date tanggalLahir, Integer masaKerja,
+			Short statusPernikahan, Date kontrakAwal, Date kontrakAkhir, String jenisKelamin, Integer jumlahAnak) {
+		super();
+		this.idKaryawan = idKaryawan;
+		this.posisiDto = posisiDto;
+		this.penempatanDto = penempatanDto;
+		this.tingkatanDto = tingkatanDto;
+		this.agamaDto = agamaDto;
+		this.nama = nama;
+		this.noKtp = noKtp;
+		this.alamat = alamat;
+		this.tanggalLahir = tanggalLahir;
+		this.masaKerja = masaKerja;
+		this.statusPernikahan = statusPernikahan;
+		this.kontrakAwal = kontrakAwal;
+		this.kontrakAkhir = kontrakAkhir;
+		this.jenisKelamin = jenisKelamin;
+		this.jumlahAnak = jumlahAnak;
+	}
+
+	public int getIdKaryawan() {
+		return idKaryawan;
+	}
+
+	public PosisiDTO getPosisiDto() {
+		return posisiDto;
+	}
+
+	public PenempatanDTO getPenempatanDto() {
+		return penempatanDto;
+	}
+
+	public TingkatanDTO getTingkatanDto() {
+		return tingkatanDto;
+	}
+
+	public AgamaDTO getAgamaDto() {
+		return agamaDto;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public String getNoKtp() {
+		return noKtp;
+	}
+
+	public String getAlamat() {
+		return alamat;
+	}
+
+	public Date getTanggalLahir() {
+		return tanggalLahir;
+	}
+
+	public Integer getMasaKerja() {
+		return masaKerja;
+	}
+
+	public Short getStatusPernikahan() {
+		return statusPernikahan;
+	}
+
+	public Date getKontrakAwal() {
+		return kontrakAwal;
+	}
+
+	public Date getKontrakAkhir() {
+		return kontrakAkhir;
+	}
+
+	public String getJenisKelamin() {
+		return jenisKelamin;
+	}
+
+	public Integer getJumlahAnak() {
+		return jumlahAnak;
+	}
+
+	public void setIdKaryawan(int idKaryawan) {
+		this.idKaryawan = idKaryawan;
+	}
+
+	public void setPosisiDto(PosisiDTO posisiDto) {
+		this.posisiDto = posisiDto;
+	}
+
+	public void setPenempatanDto(PenempatanDTO penempatanDto) {
+		this.penempatanDto = penempatanDto;
+	}
+
+	public void setTingkatanDto(TingkatanDTO tingkatanDto) {
+		this.tingkatanDto = tingkatanDto;
+	}
+
+	public void setAgamaDto(AgamaDTO agamaDto) {
+		this.agamaDto = agamaDto;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public void setNoKtp(String noKtp) {
+		this.noKtp = noKtp;
+	}
+
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+
+	public void setTanggalLahir(Date tanggalLahir) {
+		this.tanggalLahir = tanggalLahir;
+	}
+
+	public void setMasaKerja(Integer masaKerja) {
+		this.masaKerja = masaKerja;
+	}
+
+	public void setStatusPernikahan(Short statusPernikahan) {
+		this.statusPernikahan = statusPernikahan;
+	}
+
+	public void setKontrakAwal(Date kontrakAwal) {
+		this.kontrakAwal = kontrakAwal;
+	}
+
+	public void setKontrakAkhir(Date kontrakAkhir) {
+		this.kontrakAkhir = kontrakAkhir;
+	}
+
+	public void setJenisKelamin(String jenisKelamin) {
+		this.jenisKelamin = jenisKelamin;
+	}
+
+	public void setJumlahAnak(Integer jumlahAnak) {
+		this.jumlahAnak = jumlahAnak;
+	}
 	
 }
