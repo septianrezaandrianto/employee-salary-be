68cf13c4acc5cd6304191f91111f97521d48c1c5
ren skyfall <koumeharuno@gmail.com>
2020-03-30 11:02:43 +0700

    Repo Reno 1

---
+++ src/main/java/com/employee/Employee/repository/ParameterPajakRepository.java
@@ -0,0 +1,9 @@
+package com.employee.Employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.employee.Employee.model.ParameterPajak;
+
+public interface ParameterPajakRepository extends JpaRepository<ParameterPajak, Integer>{
+
+}
---
+++ src/main/java/com/employee/Employee/repository/PosisiRepository.java
@@ -0,0 +1,9 @@
+package com.employee.Employee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.employee.Employee.model.Posisi;
+
+public interface PosisiRepository extends JpaRepository<Posisi, Integer>{
+
+}
