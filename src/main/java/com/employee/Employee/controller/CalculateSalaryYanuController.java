75bee410d303f242b0b39bd0e1eee536b8a55385
Rifky Mamonto <yanoeee@gmail.com>
2020-04-05 20:03:25 +0700

    Update Controller CalculateSalary & LemburBonus

--- src/main/java/com/employee/Employee/controller/CalculateSalaryYanuController.java
+++ src/main/java/com/employee/Employee/controller/CalculateSalaryYanuController.java
@@ -1,17 +1,17 @@
 package com.employee.Employee.controller;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 import com.employee.Employee.model.Karyawan;
+import com.employee.Employee.model.LemburBonus;
 import com.employee.Employee.model.Parameter;
 import com.employee.Employee.model.Penempatan;
 import com.employee.Employee.model.PresentaseGaji;
 import com.employee.Employee.model.TunjanganPegawai;
 import com.employee.Employee.repository.KaryawanRepository;
+import com.employee.Employee.repository.LemburBonusRepository;
 import com.employee.Employee.repository.ParameterRepository;
 import com.employee.Employee.repository.PendapatanRepository;
 import com.employee.Employee.repository.PenempatanRepository;
@@ -48,6 +48,9 @@ public class CalculateSalaryYanuController {
     @Autowired
     ParameterRepository parameterRepository;
 
+    @Autowired
+    LemburBonusRepository lemburBonusRepository;
+
     ModelMapper modelMapper = new ModelMapper();
 
     // public HashMap<String, Object> getAllPendapatan(@RequestParam(name = "date") Date date) {
@@ -58,7 +61,7 @@ public class CalculateSalaryYanuController {
     //     return result;
     // }
     
-    @GetMapping("/test")
+    @GetMapping("/test/yanu")
     public HashMap<Integer, Object> calculateTakeHomePay(@RequestParam(name = "date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date requestDate) {
         HashMap<Integer, Object> a = new HashMap<Integer, Object>();
 
@@ -68,11 +71,15 @@ public class CalculateSalaryYanuController {
                 double bpjs = calculateBPJS(requestDate, calculateGajiPokok(tempKar));
                 double pph = calculatePPH(gajiKotor);
 
-                a.put(tempKar.getIdKaryawan(), bpjs);
+                a.put(tempKar.getIdKaryawan(), pph);
             }
         // }
 
         return a;
+    }
+
+    public double calculateLembur(LemburBonus lemburBonus, Parameter parameter) {
+        return Double.parseDouble(String.valueOf(lemburBonus.getLamaLembur())) * parameter.getLembur().doubleValue();
     }
 
     public boolean newMonthValidation(Date requestDate) {
@@ -90,11 +97,12 @@ public class CalculateSalaryYanuController {
     }
 
     public double calculateTunjangan(Date requestDate, double gajiPokok, Karyawan karyawan) {
-        SimpleDateFormat simDat = new SimpleDateFormat("MM");
+        SimpleDateFormat simDatM = new SimpleDateFormat("MM");
+        SimpleDateFormat simDatY = new SimpleDateFormat("yyyy");
         double result = chooseTunjanganPegawai(karyawan);
 
         for (Parameter tempPar : parameterRepository.findAll()) {
-            if (simDat.format(tempPar.getTbParameter()).equals(simDat.format(requestDate))) {
+            if (simDatM.format(tempPar.getTbParameter()).equals(simDatM.format(requestDate)) && simDatY.format(tempPar.getTbParameter()).equals(simDatY.format(requestDate))) {
                 if (karyawan.getStatusPernikahan() == 1) {
                     result += calculateTunjanganPernikahan(karyawan, tempPar);
                 }
@@ -102,6 +110,7 @@ public class CalculateSalaryYanuController {
                 if (karyawan.getPenempatan().getKotaPenempatan().equals("DKI Jakarta")) {
                     result += calculateTunjanganTransport(tempPar);
                 }
+                
                 break;
             }
         }
---
+++ src/main/java/com/employee/Employee/controller/LemburBonusYanuController.java
@@ -0,0 +1,167 @@
+package com.employee.Employee.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.validation.Valid;
+
+import com.employee.Employee.dto.LemburBonusDTO;
+import com.employee.Employee.model.LemburBonus;
+import com.employee.Employee.repository.LemburBonusRepository;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LemburBonusYanuController {
+
+    @Autowired
+    LemburBonusRepository lemburBonusRepository;
+
+    ModelMapper modelMapper = new ModelMapper();
+
+    @GetMapping("/lembur-bonus/get/all")
+    public HashMap<String, Object> getAllLemburBonus() {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        ArrayList<LemburBonusDTO> lemburBonusList = new ArrayList<LemburBonusDTO>();
+
+        for (LemburBonus tempLem : lemburBonusRepository.findAll()) {
+            lemburBonusList.add(convertToDTO(tempLem));
+        }
+
+        result.put("Message", "Success");
+        result.put("Total", lemburBonusList.size());
+        result.put("Data", lemburBonusList);
+
+        return result;
+    }
+
+    @GetMapping("/lembur-bonus/get/{id}")
+    public HashMap<String, Object> getLemburBonusById(@PathVariable(name = "id") Long id) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        result.put("Message", "Success");
+        result.put("Data", convertToDTO(lemburBonusRepository.findById(id).get()));
+
+        return result;
+    }
+
+    @PostMapping("/lembur-bonus/add")
+    public HashMap<String, Object> createLemburBonus(@Valid @RequestBody LemburBonusDTO lemburBonusDTO) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+
+        if (newLemburBonusValidation(lemburBonusDTO)) {
+            result.put("Message", "Success");
+            result.put("Data", convertToDTO(lemburBonusRepository.save(convertToEntity(lemburBonusDTO))));
+        } else {
+            result = updateLemburBonusById(getLemburBonusId(lemburBonusDTO), lemburBonusDTO);
+        }
+
+        return result;
+    }
+    
+    @PutMapping("/lembur-bonus/update/{id}")
+    public HashMap<String, Object> updateLemburBonusById(@PathVariable(name = "id") Long id, @Valid @RequestBody LemburBonusDTO lemburBonusDTO) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        LemburBonusDTO updatedLemburBonusDTO = convertToDTO(lemburBonusRepository.findById(id).get());
+
+        if (lemburBonusDTO.getIdKaryawan() != null) {
+            updatedLemburBonusDTO.setIdKaryawan(lemburBonusDTO.getIdKaryawan());
+        }
+
+        if (lemburBonusDTO.getTanggalLemburBonus() != null) {
+            updatedLemburBonusDTO.setTanggalLemburBonus(lemburBonusDTO.getTanggalLemburBonus());
+        }
+
+        if (lemburBonusDTO.getLamaLembur() > 0) {
+            updatedLemburBonusDTO.setLamaLembur(lemburBonusDTO.getLamaLembur());
+        } else {
+            updatedLemburBonusDTO.setLamaLembur(updatedLemburBonusDTO.getLamaLembur());
+        }
+        
+        if (lemburBonusDTO.getVariableBonus() > 0) {
+            updatedLemburBonusDTO.setVariableBonus(lemburBonusDTO.getVariableBonus());
+        } else {
+            updatedLemburBonusDTO.setVariableBonus(updatedLemburBonusDTO.getVariableBonus());
+        }
+
+        result.put("Message", "Success");
+        result.put("Data", convertToDTO(lemburBonusRepository.save(convertToEntity(updatedLemburBonusDTO))));
+
+        return result;
+    }
+
+    @DeleteMapping("/lembur-bonus/delete/{id}")
+    public HashMap<String, Object> deleteLemburBonusById(@PathVariable(name = "id") Long id) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        LemburBonusDTO deletedLemburBonusDTO = convertToDTO(lemburBonusRepository.findById(id).get());
+
+        lemburBonusRepository.deleteById(id);
+
+        result.put("Message", "Success");
+        result.put("Data", deletedLemburBonusDTO);
+
+        return result;
+    }
+
+    public Long getLemburBonusId(LemburBonusDTO lemburBonusDTO) {
+        SimpleDateFormat simDatM = new SimpleDateFormat("MM");
+        SimpleDateFormat simDatY = new SimpleDateFormat("yyyy");
+        Long id = null;
+
+        for (LemburBonus tempLem : lemburBonusRepository.findAll()) {
+            String month = simDatM.format(lemburBonusDTO.getTanggalLemburBonus());
+            String year = simDatY.format(lemburBonusDTO.getTanggalLemburBonus());
+
+            String temMo = simDatM.format(tempLem.getTanggalLemburBonus());
+            String temYe = simDatY.format(tempLem.getTanggalLemburBonus());
+
+            if (lemburBonusDTO.getIdKaryawan() == convertToDTO(tempLem).getIdKaryawan() && month.equalsIgnoreCase(temMo) && year.equalsIgnoreCase(temYe)) {
+                id = tempLem.getIdLemburBonus();
+                break;
+            }
+        }
+
+        return id;
+    }
+
+    public boolean newLemburBonusValidation(LemburBonusDTO lemburBonusDTO) {
+        SimpleDateFormat simDatM = new SimpleDateFormat("MM");
+        SimpleDateFormat simDatY = new SimpleDateFormat("yyyy");
+        boolean isNew = true;
+
+        for (LemburBonus tempLem : lemburBonusRepository.findAll()) {
+            String month = simDatM.format(lemburBonusDTO.getTanggalLemburBonus());
+            String year = simDatY.format(lemburBonusDTO.getTanggalLemburBonus());
+
+            String temMo = simDatM.format(tempLem.getTanggalLemburBonus());
+            String temYe = simDatY.format(tempLem.getTanggalLemburBonus());
+
+            if (lemburBonusDTO.getIdKaryawan() == convertToDTO(tempLem).getIdKaryawan() && month.equalsIgnoreCase(temMo) && year.equalsIgnoreCase(temYe)) {
+                isNew = false;
+                break;
+            }
+        }
+
+        return isNew;
+    }
+
+    public LemburBonus convertToEntity(LemburBonusDTO lemburBonusDTO) {
+        return modelMapper.map(lemburBonusDTO, LemburBonus.class);
+    }
+
+    public LemburBonusDTO convertToDTO(LemburBonus lemburBonus) {
+        return modelMapper.map(lemburBonus, LemburBonusDTO.class);
+    }
+
+}
